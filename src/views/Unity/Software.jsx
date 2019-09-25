8eeca4ff016cc853a0e64a92c55d24801e0acf0c
GreeFine <greefine@hotmail.fr>
2019-09-25 20:38:00 +0200

    Unity on feedback from save mutation

--- src/views/Unity/Software.jsx
+++ src/views/Unity/Software.jsx
@@ -30,7 +30,7 @@ class Software extends Component {
 
     this.unityContent.on('progress', progression => {
       if (progression === 1) {
-        console.log("Unity load successfully, sending initialization data", JSON.stringify(data));
+        console.log('Unity load successfully, sending initialization data', JSON.stringify(data));
         this.unityContent.send('ReactProxy', 'InitScene', JSON.stringify(data));
       }
     });
@@ -52,23 +52,23 @@ class Software extends Component {
           `}
         />
         <Mutation mutation={UPDATE_GARDEN} refetchQueries={[{ query: USER_GARDENS }]}>
-          {updateGarden => {
+          {(updateGarden, { loading, error }) => {
             this.unityContent.on('save', datas => {
-              console.log("Saving garden ...");
-              console.log("Datas received from Unity : ", datas);
+              console.log('Saving garden ...');
+              console.log('Datas received from Unity : ', datas);
 
-              console.log("Parsing data JSON.parse ...");
+              console.log('Parsing data JSON.parse ...');
               const { modifications, additions, deletions, data } = JSON.parse(datas);
               const vMod = modifications ? JSON.stringify(modifications) : '';
               const vAdd = additions ? JSON.stringify(additions) : '';
               const vDel = deletions ? JSON.stringify(deletions) : '';
 
-              console.log("Data parsed successfully");
-              console.log("Modifications: ", vMod);
-              console.log("Additions: ", vAdd);
-              console.log("Deletions: ", vDel);
+              console.log('Data parsed successfully');
+              console.log('Modifications: ', vMod);
+              console.log('Additions: ', vAdd);
+              console.log('Deletions: ', vDel);
 
-              console.log("Sending save request to the server");
+              console.log('Sending save request to the server');
               updateGarden({
                 variables: {
                   id,
@@ -79,6 +79,13 @@ class Software extends Component {
                   deletions: vDel
                 }
               });
+
+              console.log('Request errror:', error);
+              this.unityContent.send(
+                'ReactProxy',
+                'OnSaveResult',
+                JSON.stringify({ success: !error })
+              );
             });
             return null;
           }}
@@ -86,11 +93,11 @@ class Software extends Component {
         <ApolloConsumer>
           {client => {
             this.unityContent.on('query', async payload => {
-              console.log("Query received from Unity before parsing : ", payload);
-              console.log("Trying to JSON.parse the given payload...");
+              console.log('Query received from Unity before parsing : ', payload);
+              console.log('Trying to JSON.parse the given payload...');
               const parsedPayload = JSON.parse(payload);
-              console.log("Payload parsed successfully, resulting object : ", parsedPayload);
-              console.log("Sending request to the server ...");
+              console.log('Payload parsed successfully, resulting object : ', parsedPayload);
+              console.log('Sending request to the server ...');
               const { data, errors } = await client.query({
                 query: gql`
                   ${parsedPayload.query}
@@ -98,17 +105,20 @@ class Software extends Component {
                 variables: parsedPayload.variables
               });
 
-              console.log("Payload received from the server : ", data);
-              console.log("Error received : ", errors);
+              console.log('Payload received from the server : ', data);
+              console.log('Error received : ', errors);
 
-              console.log("Sending result back to Unity : ", JSON.stringify({ data: { ...data } }) || JSON.stringify(errors));
+              console.log(
+                'Sending result back to Unity : ',
+                JSON.stringify({ data: { ...data } }) || JSON.stringify(errors)
+              );
               this.unityContent.send(
                 'ReactProxy',
                 'DispatchQueryResult',
                 JSON.stringify({ data: { ...data } }) || JSON.stringify(errors)
               );
 
-              console.log("Success");
+              console.log('Success');
             });
           }}
         </ApolloConsumer>
